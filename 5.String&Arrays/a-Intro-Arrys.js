a701daeb8fd7a5ef1029fe3bd44b849be304aef4
JonathanChallco <jonathanchallco21@gmail.com>
2024-11-02 00:59:12 -0300

    String and Arry

---
+++ 5.String&Arrays/a-Intro-Arrys.js
@@ -0,0 +1,16 @@
+// Crar un Array (Arreglo)
+let edades = [24, 37, 18, 59, 7];
+let nombre = ["Juana", "Raul", "Alan"];
+let booleanos = [true, false, false. true];
+let combinados = [15, "hola", true];
+
+// Acceder a un elemento específico de un array
+console.log(edades[2]); // 0=24 - 1=37 - 2=18 - 3=59 - 4=7 
+console.log(nombre[1]);
+console.log(booleanos[0]);
+console.log(combinados[1]);
+
+// Recorrer un array
+for(let i = 0; i < edades.length ;i++){ // length devuelve la cantidad de cosas dentreo del array (Arreglo)
+
+}
